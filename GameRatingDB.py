b27c7f59c36a04b86f4decc33e168063253f8280
Exeboss02 <gabriel.abr.wik@outlook.com>
2026-05-06 01:34:54 +0200

    started working with python

---
+++ GameRatingDB.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import mysql.connector
+
+def Read_CSV(path):
+        file = pd.read_csv(path)
+        
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
